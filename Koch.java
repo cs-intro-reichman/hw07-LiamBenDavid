5d315ccae0e5ae6c9dc05af0d292072a1d0655f6
LiamBenDavid <liamkhto12@gmail.com>
2025-12-24 19:00:07 +0200

    issorted

--- Koch.java
+++ Koch.java
@@ -8,14 +8,11 @@ public class Koch {
 		//// Uncomment only one block in each test, and remember to compile
 		//// the class whenever you change the test.
 
-        /*
-		// Tests the curve function:
-		// Gets n, x1, y1, x2, y2,
-		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
+        
 		curve(Integer.parseInt(args[0]),
 			  Double.parseDouble(args[1]), Double.parseDouble(args[2]), 
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
-		*/
+	
 
 		/*
 		// Tests the snowflake function:
@@ -27,9 +24,19 @@ public class Koch {
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
-	}
+		if (n == 0){
+            StdDraw.line(x1, y1, x2, y2);
+            return;
+        }
+
+        double xA = x1 + (x2 - x1) / 3;
+        double yA = y1 + (y2 - y1) / 3;
+
+        double xD = x1 + 2 * (x2 - x1) / 3;
+        double yD = y1 + 2 * (y2 - y1) / 3;
+
+        double xC = (xA + xD) / 2 - Math.sqrt(3)
+    }
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
 	public static void snowFlake(int n) {
